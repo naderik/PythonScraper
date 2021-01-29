56e6e6f15438177ee57938117d380abeb71f9a0e
Kasra Naderi <naderi.kasra@gmail.com>
2021-01-28 23:58:43 -0500

    Fix issues and test with indeed

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ URL = "https://ca.indeed.com/jobs?q=full+stack+developer&l=Canada"
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, "html.parser")
 results = soup.find(id='resultsCol')
-jobs = results.find_all('div', class_="jobsearch-SerpJobCard unifiedRow row result clickcard")
-
-for job in jobs:
-    print(job, end='\n'*2)
+jobs = results.findAll("div", class_="jobsearch-SerpJobCard unifiedRow row result")
 
 
 if __name__ == '__main__':
+
     print('Web Scraping Started')
+    #print(results)
+    #print(jobs)
+    for job in jobs:
+        title = job.find('h2', class_='title')
+        company = job.find('div', class_='company')
+        location = job.find('div', class_='location accessible-contrast-color-location')
+        print(title)
+        print(company)
+        print(location)
 
-    # for item in links:
-    # item.text = item.find("a").text
-    # item.href = item.find("a").attrs["href"]
-
-    # if item.text and item.href:
-    #  print(item.text)
-    #  print(item.href)
